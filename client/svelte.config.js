5c918091dfb4dfcb14bf5f95b9b74aabaa1e8ee2
fiqrioemry <foemry@gmail.com>
2025-07-18 23:01:47 +0700

    update change

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,14 +1,26 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: 'app.html',    // Custom fallback
+// 			precompress: false,
+// 			strict: false
+// 		})
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'app.html',    // Custom fallback
-			precompress: false,
-			strict: false
-		})
+		adapter: adapter()
 	}
 };
 
