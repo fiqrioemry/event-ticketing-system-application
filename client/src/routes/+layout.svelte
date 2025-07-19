e52f04e505a5ffc7961e21bbe825a836c2ca5c9b
fiqrioemry <foemry@gmail.com>
2025-07-19 14:22:08 +0700

    Remove all redirect configurations

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 <script lang="ts">
 	import '../app.css';
 	import { onMount } from 'svelte';
-	import { authStore } from '$lib/stores/auth.store';
-	import * as authService from '$lib/services/auth.service';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import LayoutLoading from '$lib/components/shared/LayoutLoading.svelte';
 
@@ -15,11 +13,12 @@
 			isLoading = false;
 		}, 150); //
 	});
+	export let children;
 </script>
 
 {#if isLoading}
 	<LayoutLoading />
 {:else}
 	<Toaster position="top-right" richColors theme="light" />
-	<slot />
+	{@render children?.()}
 {/if}
